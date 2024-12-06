5d42763cc5ab5e69b29687e2021979036a28e7ef
Alex <ap2495@cam.ac.uk>
2024-12-06 14:12:31 +0000

    Finished first draft of documentation

---
+++ docs/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_rtd_theme
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'Dual Autodifferentiaton Package'
+copyright = '2024, Alexandr Prucha'
+author = 'Alexandr Prucha'
+release = '0.1.dev0+d20241205'
+autoclass_content = 'both'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.mathjax',
+              'nbsphinx']
+
+mathjax3_config = {
+    'tex2jax': {
+        'inlineMath': [['$', '$'], ['\\(', '\\)']],
+        'displayMath': [['$$', '$$'], ['\\[', '\\]']]
+    }
+}
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,57 @@
+.. DualAutodifferentiation Package documentation master file, created by
+   sphinx-quickstart on Thu Dec  5 15:28:19 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Dual Autodifferentiation Package Documentation
+==============================================
+
+Welcome to the documentation for the Dual Autodifferentiation Package! This package provides utilities for automatic differentiation.
+
+Contents
+--------
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Main Contents:
+
+   source/modules
+
+
+Module Reference
+----------------
+
+The following sections provide detailed API documentation for the modules and classes in the `dual_autodiff` package.
+
+Dual Class
+----------
+
+This section describes the `Dual` class and its methods:
+
+.. automodule:: dual_autodiff.dual
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    :no-index:
+
+Version Information
+-------------------
+
+This section documents the `dual_autodiff.version` module:
+
+.. automodule:: dual_autodiff.version
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    :no-index:
+
+
+Jupyter Notebook Example
+========================
+
+Below is an example of a Jupyter notebook demonstrating the package's usage:
+
+.. toctree::
+   :maxdepth: 1
+
+   dual_autodiff
